162acc7e620b14ee69b0e581009b070b7207b159
Jack Christensen <jack@jackchristensen.com>
2017-09-04 10:59:17 -0500

    Remove superfluous comments

--- c323_3sum/go/src/c323/main.go
+++ c323_3sum/go/src/c323/main.go
@@ -66,22 +66,3 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
-
-// while gets
-//   nums = $_.split.map { |s| Integer(s) }.sort
-
-//   answers = []
-//   (0...nums.size).each do |i|
-//     ((i+1)...nums.size).each do |j|
-//       ((j+1)...nums.size).each do |k|
-//         answers.push [nums[i], nums[j], nums[k]] if nums[i] + nums[j] + nums[k] == 0
-//       end
-//     end
-//   end
-
-//   answers.sort.uniq.each do |a|
-//     puts a.join(" ")
-//   end
-
-//   puts
-// end
